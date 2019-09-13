6b287f5c2cfd51ff7bacac7db46ca270253286c5
MrCookiez <theodorosvragkos@gmail.com>
2019-09-13 21:05:18 +0200

    logout

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, LOGIN_SUCCESS, USER_LOADED, LOGIN_FAIL, AUTH_ERROR, LOGOUT } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -17,12 +17,14 @@ export const loadUser = () => async dispatch => {
             payload: res.data
         });
 
+        dispatch(loadUser());
+
     } catch (err) {
         dispatch({
             type: AUTH_ERROR
         });
     }
-}
+};
 
 // Register User
 export const register = ({
@@ -49,6 +51,9 @@ export const register = ({
              type: REGISTER_SUCCESS,
              payload: res.data
          })
+
+         dispatch(loadUser());
+
      } catch (err) {
          const errors = err.response.data.errors;
 
@@ -60,4 +65,39 @@ export const register = ({
              type: REGISTER_FAIL
          })
      }
- }
+ };
+
+ // Login User
+export const login = (email, password) => async dispatch => {
+     const config = {
+         headers: {
+             'Content-Type': 'application/json'
+         }
+     }
+
+     const body = JSON.stringify({ email, password });
+
+     try {
+         const res = await axios.post('/api/auth', body, config);
+
+         dispatch({
+             type: LOGIN_SUCCESS,
+             payload: res.data
+         })
+     } catch (err) {
+         const errors = err.response.data.errors;
+
+         if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+         }
+
+         dispatch({
+             type: LOGIN_FAIL
+         })
+     }
+ };
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
+};
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -13,8 +16,13 @@ const Login = () => {
 
     const onSubmit = e => {
         e.preventDefault();
-        console.log('Success');
+        login(email, password);
     };
+
+    // Redirect if logged in
+    if(isAuthenticated) {
+        return <Redirect to='/dashboard' />
+    }
 
     return (
         <>
@@ -43,7 +51,7 @@ const Login = () => {
                     required
                 />
                 </div>
-                <input type="submit" className="btn btn-primary" value="Register" />
+                <input type="submit" className="btn btn-primary" value="Login" />
             </form>
             <p className="my-1">
                 Don't have an account yet? <Link to='/register'>Sign up</Link>
@@ -53,4 +61,13 @@ const Login = () => {
     );
 };
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {login})(Login);
